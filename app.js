4067a1efe0df702f8ba86a45558485f4e33ecf42
pavan-reddy-28 <35028241+pavan-reddy-28@users.noreply.github.com>
2023-11-27 03:29:03 +0000

    db changes

--- app.js
+++ app.js
@@ -2,16 +2,47 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer  = require('multer');
+const mysql = require('mysql');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const upload = multer();
 
-app.get("/hello",upload.any(), async (req,res) => {
-    
-    res.json({data:"hello"});
-})
+const pool = mysql.createPool({
+    connectionLimit: 10, // adjust as per your requirement
+    host: 'matrix-database.cepm8nx3leyv.us-east-1.rds.amazonaws.com',
+    user: 'rdsUser',
+    password: 'admin12345',
+    database: 'matrixDB',
+  });
+
+
+
+app.get('/hello', (req, res) => {
+    // Get the database name from the RDS instance
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.error('Error connecting to database:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+  
+      // Execute query to fetch the database name
+      connection.query('SELECT DATABASE() AS dbName', (error, results) => {
+        connection.release(); // Release the connection
+  
+        if (error) {
+          console.error('Error executing query:', error);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+  
+        const dbName = results[0].dbName;
+        res.send(`Database name is: ${dbName}`);
+      });
+    });
+});
 
 
 app.listen(8000, () => {
