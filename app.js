44caebbbd6ea5dc5fe3a1a6daba1391444aaab90
pavan-reddy-28 <35028241+pavan-reddy-28@users.noreply.github.com>
2023-11-29 07:21:10 +0000

    dynamo update

--- app.js
+++ app.js
@@ -2,21 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer  = require('multer');
-const mysql = require('mysql');
+const crypto = require('crypto');
+const AWS = require('aws-sdk');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const upload = multer();
 
-const pool = mysql.createPool({
-    connectionLimit: 10, // adjust as per your requirement
-    host: 'matrix-database.cepm8nx3leyv.us-east-1.rds.amazonaws.com',
-    user: 'rdsUser',
-    password: 'admin12345',
-    database: 'matrixDB',
-  });
+AWS.config.update({
+  region: 'us-east-1'
+});
 
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
+
+const uniqueID = crypto.randomBytes(16).toString('hex');
 
 
 app.post('/hello', (req, res) => {
@@ -50,26 +50,13 @@ app.post('/hello', (req, res) => {
     comments
   } )
     // Get the database name from the RDS instance
-    pool.getConnection((err, connection) => {
-      if (err) {
-        console.error('Error connecting to database:', err);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-      const insertQuery = `
-      INSERT INTO userInfo 
-      (first_name, last_name, Email, Confirm_email, Mobile_number, street, Apt, city, State, Pincode, Country, comments) 
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `;
-      // Execute query to fetch the database name
-     // Insert the form data into the table
-    connection.query(
-      insertQuery,
-      [
+    const params = {
+      TableName: 'matrixUserInfoDynamo',
+      Item: {
+        customer_id: uniqueID,
         first_name,
         last_name,
         Email,
-        Confirm_email,
         Mobile_number,
         street,
         Apt,
@@ -78,19 +65,17 @@ app.post('/hello', (req, res) => {
         Pincode,
         Country,
         comments
-      ],
-      (queryErr, results) => {
-        // Release the connection back to the pool
-        connection.release();
+      }
+    };
 
-        if (queryErr) {
-          console.error('Error inserting data:', queryErr);
-          res.status(500).send('Error inserting data');
-          return;
-        }
-
-        res.status(200).send('Data inserted successfully!');
-      });
+    dynamoDB.put(params, (err, data) => {
+      if (err) {
+        console.error('Error inserting data into DynamoDB:', err);
+        res.status(500).send('Error inserting data into DynamoDB');
+      } else {
+        console.log('Data inserted into DynamoDB:', data);
+        res.status(200).send('Data inserted into DynamoDB');
+      }
     });
 });
 
